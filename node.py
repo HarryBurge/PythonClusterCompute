1d4af753365761b20ecb045c4bb6316d08e0a19e
Hightorean <burgeharry@gmail.com>
2021-05-31 20:36:11 +0100

    .

--- node.py
+++ node.py
@@ -16,7 +16,27 @@ class Node:
         self.network= network
         self.ip= ip
         self.connections['20000']= None
+
+
+    def __str__(self):
+        line= 'Node {}: ['.format(self.ip)
+
+        for k,d in self.connections.items():
+
+            if d!=None:
+                line+= '{}-{}:{},'.format(k, d[0], d[1])
+            else:
+                line+= 'Listening'
+
+        return line+']'
     
 
     def run(self):
-        pass
+
+        self.connections['20001']= None
+        
+        # while True:
+        for i in range(1):
+            
+            if self.network.connect_node_to_node(self.ip, '20001', '192.168.0.0', '20000'):
+                return True
--- sim.py
+++ sim.py
@@ -7,12 +7,16 @@ __doc__ = 'Creates a simulation of a running cluster'
 from node import Node
 
 import threading
+import pygame
+import math
 
 
 #~ Sim
 class Sim:
     
     nodes= {}
+    screen= pygame.display.set_mode((500, 500))
+    clock= pygame.time.Clock()
 
     def __init__(self):
         # Init nodes
@@ -27,13 +31,51 @@ class Sim:
             t.setDaemon= True
             t.start()
 
+        # Drawing
+        diff_angle= 2*math.pi/len(self.nodes.keys())
+        s_colour= (255, 255, 0)
+        e_colour= (255, 255, 255)
 
+        n_colour= (0, 0, 255)
+
+        def rotate(angle):
+            s = math.sin(angle)
+            c = math.cos(angle)
+            return (250 + 150*c - 0*s, 250 + 150*s + 0*c)
+
+        while True:
+            self.screen.fill((0,0,0))
+
+            # Draw connections
+            for k,d in self.nodes.items():
+                for pk,pd in d.connections.items():
+
+                    if pd!=None:
+                        start= int(k.split('.')[-1])
+                        end= int(pd[0].split('.')[-1])
+
+                        if pk=='20000' or pk=='20001':
+                            pygame.draw.line(self.screen, s_colour, rotate(start*diff_angle), rotate(end*diff_angle), 4)
+                        else:
+                            pygame.draw.line(self.screen, e_colour, rotate(start*diff_angle), rotate(end*diff_angle), 4)
+
+            # Draw nodes
+            for k,d in self.nodes.items():
+                ind= int(k.split('.')[-1])
+                pygame.draw.circle(self.screen, n_colour, rotate(ind*diff_angle), 20)
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+            print(self.nodes['192.168.0.0'])
+
+ 
     def connect_node_to_node(self, ip1, port1, ip2, port2):
         # Check both ports are clear then connect them
         try:
             if self.nodes[ip1].connections[port1]==None and self.nodes[ip2].connections[port2]==None:
                 self.nodes[ip1].connections[port1]= (ip2, port2, [])
-                self.nodes[ip2].connections[port2]= (ip1, port2, [])
+                self.nodes[ip2].connections[port2]= (ip1, port1, [])
                 return True
 
         # If either port doesn't exist
@@ -50,5 +92,6 @@ class Sim:
 
 
 if __name__=='__main__':
+    pygame.init()
     sim= Sim()
     sim.run()
