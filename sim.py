d69a4815e2c7b7f6c0e27a1d75a7078670e60862
Harry Burge <Burgeharry@gmail.com>
2021-07-01 17:11:51 +0100

    .

--- sim.py
+++ sim.py
@@ -1,6 +1,8 @@
 #~ Imports
+from cluster_node import Node
 
 #~ Constants
+NUM_NODES= 3
 
 #~ Sim
 class Sim:
@@ -9,10 +11,32 @@ class Sim:
 
         self.nodes= {}
 
+        for i in range(NUM_NODES):
+            self.nodes[f'192.168.1.{i}']= Node(f'192.168.1.{i}')
 
-    def tcp(self, tarip, tarport):
-        pass
+
+    def tcp(self, tarip, tarport, msg):
+        
+        if (
+            tarip in self.nodes.keys() and 
+            tarport in self.nodes[tarip].ports.keys() and 
+            self.nodes[tarip].ports[tarport].targetip != None and 
+            self.nodes[tarip].ports[tarport].targetport != None
+        ):
+            self.nodes[tarip].ports[tarport].receive(msg)
+        else:
+            return False
 
 
     def run(self):
-        pass
+        
+        while True:
+            for k,d in self.nodes.items():
+                d.step()
+
+
+
+
+if __name__ == '__main__':
+    sim= Sim()
+    sim.run()
--- sim_port.py
+++ sim_port.py
@@ -5,17 +5,27 @@
 #~ Port
 class Port:
 
-    def __init__(self, portnum):
+    def __init__(self, portnum, network):
         
         self.portnum= portnum
+        self.network= network
+
         self.targetip= None
         self.targetport= None
         self.buffer= []
 
     
     def read(self):
-        pass
+        if (len(self.buffer) != 0):
+            return self.buffer.pop(0)
+        return False
 
 
     def send(self, msg):
-        pass
+        if (self.network.tcp(self.targetip, self.targetport, msg) != False):
+            return True
+        return False
+
+
+    def receive(self, msg):
+        self.buffer.append(msg)
